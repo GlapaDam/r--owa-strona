ae4098ae0164abcaa4dc8ae1ff4c63f6ec6c1925
damian3681 <72092341+damian3681@users.noreply.github.com>
2020-11-10 15:13:25 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+$('.slider .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    items: 1,
+})
+
+$('.testimonials .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+
+AOS.init();
